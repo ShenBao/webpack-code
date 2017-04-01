029bdd46d699139d786965f5c29a7b6f6d9985cf
shenbao <shenbaoone@gmail.com>
2017-04-01 21:07:25 +0800

    add demo12

---
+++ demo12/bundle1.js
@@ -0,0 +1,19 @@
+webpackJsonp([1],{
+
+/***/ 81:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var React = __webpack_require__(32);
+var ReactDOM = __webpack_require__(31);
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello World'
+), document.getElementById('a'));
+
+/***/ })
+
+},[81]);
---
+++ demo12/bundle2.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],{
+
+/***/ 82:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var React = __webpack_require__(32);
+var ReactDOM = __webpack_require__(31);
+ReactDOM.render(React.createElement(
+  'h2',
+  null,
+  'Hello Webpack'
+), document.getElementById('b'));
+
+/***/ })
+
+},[82]);
