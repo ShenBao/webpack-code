aca0020575ea82c1b0328bb69e1cc2eaba76db86
shenbao <shenbaoone@gmail.com>
2017-04-01 19:07:10 +0800

    updata files ...

---
+++ demo05/main.js
@@ -0,0 +1,9 @@
+var img1 = document.createElement("img");
+img1.src = require("./small.png");
+img1.width = 400;
+document.body.appendChild(img1);
+
+var img2 = document.createElement("img");
+img2.src = require("./big.png");
+img2.width = 400;
+document.body.appendChild(img2);
